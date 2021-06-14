6fcd2e92b4586040f56676d521476c18680a6d14
PythonNurs <nursultandev@gmail.com>
2021-06-14 06:09:46 +0300

    add widgets for Product model

--- apps/products/forms.py
+++ apps/products/forms.py
@@ -1,9 +1,24 @@
-from apps.products.models import Product
-from django.forms import ModelForm
+from apps.products.models import Product, ProductImage
+from django import forms
 
 
-class ProductForm(ModelForm):
-
+class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['title', 'description', 'price', 'quantity', 'is_stock']
+        exclude = ['is_stock', ]
+        widgets = {
+            'title': forms.TextInput(attrs={'class': "form-control"}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'quantity': forms.NumberInput(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+        }
+
+
+class ProductImageForm(forms.ModelForm):
+    class Meta:
+        model = ProductImage
+        fields = ['image', ]
+        widgets = {
+            'image': forms.ClearableFileInput(attrs={'class': 'form-control'})
+        }
--- apps/products/models.py
+++ apps/products/models.py
@@ -25,3 +25,18 @@ class Product(models.Model):
 
     def __str__(self):
         return f"{self.title} -- {self.is_stock}"
+
+
+class ProductImage(models.Model):
+    image = models.ImageField(
+        upload_to='product',
+        verbose_name='Картинки',
+        blank=True, null=True
+    )
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE,
+        related_name='product_image'
+    )
+
+    def __str__(self):
+        return f"{self.product.title} -- {self.product.id}"
